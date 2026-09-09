a3309cd6c3e5118b80d7440f9ac120147c1ed125
NishantG12345 <nishantganesan123@gmail.com>
2026-09-09 19:05:56 -0400

    completed window for now

--- RTL/computation.v
+++ RTL/computation.v
@@ -1,5 +1,6 @@
 module computation
     (
+    input valid_in;
     input  [7:0] pixel00,pixel01,pixel02,
     input  [7:0] pixel10,pixel11,pixel12, 
     input  [7:0] pixel20,pixel21,pixel22,
@@ -15,18 +16,21 @@ reg signed[10:0] p21;
 reg signed[10:0] p22;
 
 always @(*) begin
-    //to ensure proper signed operations must convert to signed first
-    p00 = {3'b000,pixel00};
-    p01 = {3'b000,pixel01};
-    p02 = {3'b000,pixel02};
-    p20 = {3'b000,pixel20};
-    p21 = {3'b000,pixel21};
-    p22 = {3'b000,pixel22};
-    wide_pixel = -p00 - (p01 << 1) - p02  +  p20 + (p21 << 1) + p22;
-    if (wide_pixel < 0)
-    wide_pixel = -wide_pixel;
-    if (wide_pixel > 255)
-    wide_pixel = 255;
-    pixel_out = wide_pixel;
+    //to ensure proper signed operations must convert to signed reg first
+    if(valid_in) begin
+        p00 = {3'b000,pixel00};
+        p01 = {3'b000,pixel01};
+        p02 = {3'b000,pixel02};
+        p20 = {3'b000,pixel20};
+        p21 = {3'b000,pixel21};
+        p22 = {3'b000,pixel22};
+        wide_pixel = -p00 - (p01 << 1) - p02  +  p20 + (p21 << 1) + p22;
+        if (wide_pixel < 0)
+        wide_pixel = -wide_pixel;
+        if (wide_pixel > 255) begin
+        wide_pixel = 255;
+        pixel_out = wide_pixel;
+        end
+    end
 end
 endmodule
